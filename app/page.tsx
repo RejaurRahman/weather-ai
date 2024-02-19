2cf1f9b8938ac1adf770a03570cb42a81073ccdf
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2024-02-19 00:22:22 +0000

    ADD: ui structure tremor component for default home

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,39 @@
+"use client"
+
+import {
+  Card,
+  Divider,
+  Subtitle,
+  Text
+} from "@tremor/react"
+
 export default function Home() {
   return (
-    <main>
-      <p>Weather AI app</p>
-    </main>
+    <div
+      className="min-h-screen bg-gradient-to-br from-[#be185d] to-[#9d174d] p-10 flex flex-col justify-center items-center"
+    >
+      <Card
+        className="bg-white max-w-4xl mx-auto rounded-lg"
+      >
+        <Text
+          className="text-6xl font-bold text-center mb-10"
+        >
+          Weather AI
+        </Text>
+        <Subtitle
+          className="text-xl text-center"
+        >
+          Powered by OpenAI
+        </Subtitle>
+        <Divider
+          className="my-10 bg-cab-sarv"
+        />
+        <Card
+          className="bg-gradient-to-br from-[#be185d] to-[#9d174d] text-white rounded-2xl"
+        >
+          <p>City Picker</p>
+        </Card>
+      </Card>
+    </div>
   )
 }
