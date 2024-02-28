746478b439af6fda9fbec9a89187641959e36e93
Rejaur Rahman <rejaur-rahman@hotmail.co.uk>
2024-02-28 02:02:29 +0000

    UPDATE: fetchweatherquery to display data on location page

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -1,3 +1,7 @@
+import { getClient } from "@/apollo-client"
+
+import fetchWeatherQuery from "@/graphql/queries/fetchWeatherQueries"
+
 type Props = {
   params: {
     city: string
@@ -6,13 +10,27 @@ type Props = {
   }
 }
 
-export default function WeatherPage({
+export default async function WeatherPage({
   params: {
     city,
     lat,
     long
   }
 }: Props) {
+  const client = getClient()
+
+  const { data } = await client.query({
+    query: fetchWeatherQuery,
+    variables: {
+      current_weather: "true",
+      longitude: long,
+      latitude: lat,
+      timezone: "GMT"
+    }
+  })
+
+  const results: Root = data.myQuery
+
   return (
     <p>Welcome to the weather page: {city} {lat} {long}</p>
   )
