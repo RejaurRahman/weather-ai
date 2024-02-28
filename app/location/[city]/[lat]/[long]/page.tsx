ce3d6d0f943bfe16adc4d5c7ace5022c5ba12e98
Rejaur Rahman <rejaur-rahman@hotmail.co.uk>
2024-02-28 02:12:38 +0000

    ADD: last updated content from results data on location page

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -32,6 +32,21 @@ export default async function WeatherPage({
   const results: Root = data.myQuery
 
   return (
-    <p>Welcome to the weather page: {city} {lat} {long}</p>
+    <div>
+      <div>
+        <div className="p-5">
+          <div className="pb-5">
+            <h2 className="text-xl font-bold">
+              Today&nbsp;s Overview
+            </h2>
+            <p className="text-sm text-gray-400">
+              Last Updated at: {" "}
+              {new Date(results.current_weather.time).toLocaleString()}
+              ({results.timezone})
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
