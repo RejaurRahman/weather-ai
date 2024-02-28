19510ba0362ae644be5c1aee79503d361ab4d5b0
Rejaur Rahman <rejaur-rahman@hotmail.co.uk>
2024-02-28 12:34:36 +0000

    UPDATE: information panel complete functionality

--- components/InformationPanel/InformationPanel.tsx
+++ components/InformationPanel/InformationPanel.tsx
@@ -34,7 +34,7 @@ export default function InformationPanel({
       <CityPicker />
       <hr className="my-10" />
       <div className="mt-5 flex items-center justify-between space-x-10 mb-5">
-        <div>
+        <>
           <p className="text-xl">
             {
               new Date().toLocaleDateString("en-GB", {
@@ -48,7 +48,7 @@ export default function InformationPanel({
           <p className="font-extralight">
             Timezone: {Intl.DateTimeFormat().resolvedOptions().timeZone}
           </p>
-        </div>
+        </>
         <p className="text-xl font-bold uppercase">
           {
             new Date().toLocaleDateString("en-GB", {
@@ -60,14 +60,54 @@ export default function InformationPanel({
         </p>
       </div>
       <hr className="mt-10 mb-5" />
-      <div>
-        <div>
-          <div>
-            <p>
-              {results.current_weather.temperature.toFixed(1)}&deg;C
+      <div className="flex items-center justify-between">
+        <Image
+          alt={weatherCodeToString[results.current_weather.weathercode].label}
+          height={75}
+          src={`https://www.weatherbit.io/static/img/icons/${weatherCodeToString[results.current_weather.weathercode].icon}`}
+          width={75}
+        />
+        <div className="flex items-center justify-between space-x-10">
+          <p className="text-6xl font-semibold">
+            {results.current_weather.temperature.toFixed(1)}&deg;C
+          </p>
+          <p className="text-lg text-right font-extralight">
+            {weatherCodeToString[results.current_weather.weathercode].label}
+          </p>
+        </div>
+      </div>
+      <div className="space-y-2 py-5">
+        <div className="flex items-center space-x-2 px-4 py-3 border border-[#be185d] rounded-md bg-[#9d174d]">
+          <SunIcon
+            className="h-10 w-10 text-gray-400"
+          />
+          <div className="flex-1 flex justify-between items-center">
+            <p className="font-extralight">Sunrise</p>
+            <p className="uppercase text-2xl">
+              {
+                new Date(results.daily.sunrise[0]).toLocaleDateString("en-GB", {
+                  hour: "numeric",
+                  minute: "numeric",
+                  hour12: true
+                })
+              }
             </p>
-            <p>
-              {weatherCodeToString[results.current_weather.weathercode].label}
+          </div>
+        </div>
+        <div className="flex items-center space-x-2 px-4 py-3 border border-[#be185d] rounded-md bg-[#9d174d]">
+          <MoonIcon
+            className="h-10 w-10 text-gray-400"
+          />
+          <div className="flex-1 flex justify-between items-center">
+            <p className="font-extralight">Sunset</p>
+            <p className="uppercase text-2xl">
+              {
+                new Date(results.daily.sunset[0]).toLocaleDateString("en-GB", {
+                  hour: "numeric",
+                  minute: "numeric",
+                  hour12: true
+                })
+              }
             </p>
           </div>
         </div>
