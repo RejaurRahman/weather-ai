5ba754c9862b00438c28f12798fe850e404c8305
Rejaur Rahman <rejaur-rahman@hotmail.co.uk>
2024-02-29 00:58:28 +0000

    UPDATE: chatgpt data to display in page location summary

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -9,6 +9,9 @@ import TempChart from "@/components/TempChart/TempChart"
 
 import fetchWeatherQuery from "@/graphql/queries/fetchWeatherQueries"
 
+import cleanData from "@/lib/cleanData"
+import getBasePath from "@/lib/getBasePath"
+
 export const revalidate = 60
 
 type Props = {
@@ -40,6 +43,21 @@ export default async function WeatherPage({
 
   const results: Root = data.myQuery
 
+  const dataToSend = cleanData(results, city)
+
+  const res = await fetch(`${getBasePath()}/api/getWeatherSummary`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      weatherData: dataToSend
+    })
+  })
+
+  const GPTdata = await res.json()
+  const { content } = GPTdata
+
   return (
     <div className="flex flex-col min-h-screen md:flex-row">
       <InformationPanel 
@@ -62,7 +80,7 @@ export default async function WeatherPage({
           </div>
           <div className="m-2 mb-10">
             <CalloutCard 
-              message="This is where GPT-4 Summary will go!"
+              message={content}
             />
           </div>
           <div className="grid grid-cols-1 xl:grid-cols-2 gap-5 m-2">
