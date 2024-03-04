59c830c242b291819e931e5a03444aaa55448f9d
Rejaur Rahman <rejaur-rahman@hotmail.co.uk>
2024-03-04 14:05:48 +0000

    FIX: base path production url in helper file

--- lib/getBasePath.ts
+++ lib/getBasePath.ts
@@ -1,5 +1,5 @@
 const getBasePath = () => {
-  let base_url = process.env.NODE_ENV === "development" ? "http://localhost:3000" : `https://${process.env.PRODUCTION_URL}`
+  let base_url = process.env.NODE_ENV === "development" ? "http://localhost:3000" : process.env.PRODUCTION_URL
 
   return base_url
 }
